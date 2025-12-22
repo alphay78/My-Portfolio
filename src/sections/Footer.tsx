8fdbf45604664ff9977235e6f3df24533c392dce
Alpha <148535861+alphay78@users.noreply.github.com>
2025-12-22 13:48:58 +0300

    feat: Full Portfolio code

---
+++ src/sections/Footer.tsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import ArrowUpRightIcon from "../../public/icons/arrow-up-right.svg";
+
+const footerLinks = [
+  {
+    title: "Github",
+    href: "https://github.com/alphay78?tab=overview&from=2025-12-01&to=2025-12-19",
+  },
+  {
+    title: "Telegram",
+    href: "https://t.me/HolisticDeveloper",
+  },
+  {
+    title: "Phone",
+    href: "tel:+25184746454",
+  },
+  {
+    title: "Email",
+    href: "lond2601@gmail.com",
+  },
+  {
+    title: "Linkedin",
+    href: "https://www.linkedin.com/in/alpha-israel-023197294/",
+  },
+  // {
+  //   title: "Twitter",
+  //   href: "https://x.com/itsyaba",
+  // },
+];
+
+export const Footer = () => {
+  return (
+    <footer className="relative  overflow-x-clip">
+      <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10" />
+      <div className="container">
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
+          <div className="text-white/40">
+            &copy; {new Date().getFullYear()} Alpha Degago. All rights
+            reserved.
+          </div>
+          <nav className="grid grid-cols-3 md:grid-cols-6 gap-4">
+            {footerLinks.map((link) => (
+              <Link
+                target="_blank"
+                href={link.href}
+                key={link.title}
+                className="inline-flex items-center gap-1.5 cursor-pointer hover:underline hover:text-white/80 transition-all"
+              >
+                <span className="font-semibold">{link.title}</span>
+                <ArrowUpRightIcon className="size-4" />
+              </Link>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
+};
